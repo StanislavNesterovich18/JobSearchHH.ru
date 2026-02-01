f91f1dda284bb4e19987d74bac1969e95656e44f
S.Nesterovich <stas.nester18@mail.ru>
2026-02-01 12:44:16 +0300

    Исправление абстрактного класса

--- src/cls_api_parse_hh.py
+++ src/cls_api_parse_hh.py
@@ -6,7 +6,7 @@ from requests import Response
 
 class BaseHH(ABC):
     @abstractmethod
-    def load_vacancies(self, keyword: str) -> None: ...
+    def load_vacancies(self, keyword: str) -> list[dict]: ...
 
 
 class HH(BaseHH):
@@ -14,16 +14,23 @@ class HH(BaseHH):
     Класс для работы с API HeadHunter
     """
 
-    BASE_URL: str = "https://api.hh.ru"
-    BASE_HEADERS: dict = {"User-Agent": "HH-User-Agent"}
+    __BASE_URL: str = "https://api.hh.ru"
+    __BASE_HEADERS: dict = {"User-Agent": "HH-User-Agent"}
 
     def __init__(self) -> None:
         self.vacancies: list = []
 
-    def load_vacancies(self, keyword: str) -> None:
+    def load_vacancies(self, keyword: str) -> list[dict]:
         params: dict = {"text": keyword, "page": 0, "per_page": 100}
         while params.get("page") != 1:
-            response: Response = requests.get(f"{self.BASE_URL}/vacancies", headers=self.BASE_HEADERS, params=params)
+            response: Response = requests.get(f"{HH.__BASE_URL}/vacancies", headers=HH.__BASE_HEADERS, params=params)
             vacancies: list = response.json()["items"]
             self.vacancies.extend(vacancies)
             params["page"] += 1
+        return self.vacancies
+
+
+if __name__ == "__main__":
+    hh = HH()
+    hh.load_vacancies("python")
+    print(hh.vacancies)
